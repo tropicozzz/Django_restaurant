82d0999c90ae3e0b6c101687fd21c673c85d579b
benjamin <fuentesbenjamin978@gmail.com>
2024-07-06 00:20:25 -0400

    implementando cruds

--- adminRestaurant/urls.py
+++ adminRestaurant/urls.py
@@ -1,7 +1,28 @@
 from django.urls import path
-from .views import index
+from .views import index,inicio,empleados,empleadosadd,empleados_del,empleados_findEdit,empleadosupdate,inventario,inventarioadd,inventario_del,inventario_findEdit,inventarioupdate,gastos,gastosadd,gastos_del,gastos_finEdit,gastosupdate
 
 urlpatterns =  [
-    path ('',index,name ='index')
+    path ('',inicio,name ='inicio'),
+    path ('adminRestaurant/inventario',inventario,name = 'inventario'), #crud inventario
+    path ('adminRestaurant/inventario',inventarioadd,name = 'inventarioadd'),
+    path ('adminRestaurant/inventario',inventario_del,name = 'inventario_del'),
+    path ('adminRestaurant/inventario',inventario_findEdit,name = 'inventario_findEdit'),
+    path ('adminRestaurant/inventario',inventarioupdate,name = 'inventarioupdate'),
+
+    path ('adminRestaurant/empleados',empleados,name = 'empleados'),  #crud empleados
+    path ('adminRestaurant/empleados',empleadosadd,name = 'empleadosadd'),
+    path ('adminRestaurant/empleados',empleados_del,name = 'empleados_del'),
+    path ('adminRestaurant/empleados',empleados_findEdit,name = 'empleados_findEdit'),
+    path ('adminRestaurant/empleados',empleadosupdate,name = 'empleadosupdate'),
+
+    path ('adminRestaurant/gastos',gastos,name = 'gastos'), #crud gastos
+    path ('adminRestaurant/gastos',gastosadd,name = 'gastosadd'),
+    path ('adminRestaurant/gastos',gastos_del,name = 'gastos_del'),
+    path ('adminRestaurant/gastos',gastos_finEdit,name = 'gastos_finEdit'),
+    path ('adminRestaurant/gastos',gastosupdate,name = 'gastosupdate'),
+
+
+    path ('adminRestaurant/index',index,name = 'index')
+
 
 ]
--- adminRestaurant/views.py
+++ adminRestaurant/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+
+    return render(request,'adminRestaurant/inicio.html')
+
+def gastos(request):
+
+    return render(request,'adminRestaurant/gastos.html')
+
 def index(request):
 
     return render(request,'adminRestaurant/index.html')
+
+def empleados(request):
+
+    return render(request,'adminRestaurant/empleados.html')
+
+def inventario(request):
+
+    return render(request,'adminRestaurant/inventario.html')
